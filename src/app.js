6c1c893f67cff95220972716b9a60724ffe56871
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2021-06-12 10:52:11 +0400

    ALOT OF CHANGES

---
+++ src/app.js
@@ -0,0 +1,31 @@
+function check() {
+  // Selecting the input element and get its value
+  let a = document.getElementById("inputId").value;
+  let b = document.getElementById("password").value;
+  if (a == "Aahil" && b == "s8626") {
+    localStorage.name = a;
+    window.location.href = "Main File for Members.html";
+  } else if (a == "Aamir" && b == "s8872") {
+    localStorage.name = a;
+    window.location.href = "Main File for Members.html";
+  } else if (a == "Aashray" && b == "s12901") {
+    localStorage.name = a;
+    window.location.href = "Main File for Members.html";
+  } else if (a == "Adwaith" && b == "s10832") {
+    localStorage.name = a;
+    window.location.href = "Main File for Members.html";
+  } else if (a == "Ashar" && b == "s13490") {
+    localStorage.name = a;
+    window.location.href = "Main File for Members.html";
+  } else if (a == "Ashaz" && b == "s16166") {
+    localStorage.name = a;
+    window.location.href = "Main File for Members.html";
+  } else if (a == "Aneesh" && b == "s8376") {
+    localStorage.name = a;
+    window.location.href = "Main File for Members.html";
+  } else if (a == "Arnav" && b == "s8395") {
+    localStorage.name = a;
+    window.location.href = "Main File for Members.html";
+  } else if (a == "Atharv" && b == "s8388") {
+    localStorage.name = a;
+}
