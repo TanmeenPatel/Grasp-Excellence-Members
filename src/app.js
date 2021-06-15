2a10a38d672db1201947d572b06e55d5b7f05c49
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2021-06-16 00:03:21 +0400

    fixed all temporary and permanent issues

--- src/app.js
+++ src/app.js
@@ -2,34 +2,36 @@ function check() {
   // Selecting the input element and get its value
   let a = document.getElementById("inputId").value;
   let b = document.getElementById("password").value;
-  if (a == "Grasp_Excellence_Members" && b == "") {
+  if (a == "Grasp_Excellence_Members" && b == "AdminOnly3") {
     localStorage.name = a;
-    window.location.href = "Main File for Members.html";
+    window.location.href = "./Main File for Members.html";
   } else if (a == "Grasp_Excellence_Members" && b == "Aahil.R") {
     localStorage.name = a;
-    window.location.href = "Main File for Members.html";
+    window.location.href = "./Main File for Members.html";
   } else if (a == "Grasp_Excellence_Members" && b == "Arnav.D") {
     localStorage.name = a;
-    window.location.href = "Main File for Members.html";
+    window.location.href = "./Main File for Members.html";
   } else if (a == "Grasp_Excellence_Members" && b == "Ahammad.A") {
     localStorage.name = a;
-    window.location.href = "Main File for Members.html";
+    window.location.href = "./Main File for Members.html";
   } else if (a == "Grasp_Excellence_Members" && b == "Ashaz.O") {
     localStorage.name = a;
-    window.location.href = "Main File for Members.html";
+    window.location.href = "./Main File for Members.html";
   } else if (a == "Grasp_Excellence_Members" && b == "Krish.N") {
     localStorage.name = a;
-    window.location.href = "Main File for Members.html";
+    window.location.href = "./Main File for Members.html";
   } else if (a == "Grasp_Excellence_Members" && b == "Satvik.G") {
     localStorage.name = a;
-    window.location.href = "Main File for Members.html";
+    window.location.href = "./Main File for Members.html";
   } else if (a == "Grasp_Excellence_Members" && b == "Tanmeen.P") {
     localStorage.name = a;
-    window.location.href = "Main File for Members.html";
+    window.location.href = "./Main File for Members.html";
   } else if (a == "Grasp_Excellence_Members" && b == "Shiv.G") {
     localStorage.name = a;
-}else if (a == "Grasp_Excellence_Members" && b == "AdminOnly3") {
-  localStorage.name = a;
-}else {
+    window.location.href = "./Main File for Members.html";
+  } else if (a == "Grasp_Excellence_Members" && b == "AdminOnly3") {
+    localStorage.name = a;
+    window.location.href = "Main File for Members.html";
+  } else 
   alert("You are not a member, please contact an admin for further queries.");
 }
